c8cccd7dca6ab8c20eb35d0492537d7928c2b6c9
Luana <luavillem@gmail.com>
2025-07-25 13:19:58 -0300

    Adiciona versão responsiva do site

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 
-document.getElementById ("botao-enviar").addEventListener("click", ValidaFormulario)
-
-function ValidaFormulario (event) {
-    event.preventDefaut ();
-
-    if(document.getElementById ("nome").value != "" && document.getElementById ("email").value != "") {
-        alert ("Pronto! Você receberá o material por email")
-    }else { 
-        alert ("Preencha os campos de nome e email")
-
-    }
-}
+ document.querySelector('.menu-toggle').addEventListener('click', function () {
+  document.querySelector('nav ul').classList.toggle('active');
+});
